c6edd03922213e252982957c496e0038a6464f77
gnmmarechal <gnmpolicemata91@gmail.com>
2017-01-20 22:24:09 +0000

    Added license to scripts

--- index-server.lua
+++ index-server.lua
@@ -1,6 +1,20 @@
 --Corbenik/Skeith CFW Updater: RE - Server Script (CURE)
 --Author: gnmmarechal
 --Runs on Lua Player Plus 3DS
+--[[
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
 serverrel = 2
 version = "1.2.1"
 if devmode == 1 then -- This will differentiate between stable and devscripts.
--- index.lua
+++ index.lua
@@ -1,7 +1,20 @@
 --Corbenik/Skeith CFW Updater: RE - Client Script (CORE)
 --Author: gnmmarechal
 --Runs on Lua Player Plus 3DS
+--[[
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
 -- This script fetches the latest updater script and runs it. If the server-side script has a higher rel number, the CIA will also be updated.
 clientrel = 2
 bootstrapver = "1.0.3R2"
